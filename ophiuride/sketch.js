f3d786a1c60f639195c0bd2e813f09c229de3559
Kathy <65121394+kfahn22@users.noreply.github.com>
2023-05-10 19:33:06 -0400

    update colors

--- ophiuride/sketch.js
+++ ophiuride/sketch.js
@@ -25,44 +25,43 @@ let a = 1.5;
 let b = 0.2;
 let sc = 30;
 let oph1;
-let n = 220;
+let n = 120;
 
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 400);
   angleMode(DEGREES);
-  let c1 = color(253, 245, 191, 180);
-  let c2 = color(180,126,179, 150);
-  //let c1 = color(255, 213, 255, 100);
-  let c3 = color(146, 209, 195, 75);
-  let c4 = color(17,157,164, 50);
-  // let c1 = color(239, 195, 245, 180);
-  // let c2 = color(250, 166, 255, 100);
-  // let c3 = color(115, 83, 186, 60);
-  // let c4 = color(47, 25, 95, 20);
-  for (let i = 0; i < 0.15*n; i++)
+  // let c1 = color(253, 245, 191, 180);
+  // let c2 = color(180,126,179, 150);
+  // let c3 = color(146, 209, 195, 75);
+  // let c4 = color(17,157,164, 50);
+  let c1 = color(239, 195, 245, 180);
+  let c2 = color(250, 166, 255, 150);
+  let c3 = color(115, 83, 186, 75);
+  let c4 = color(47, 25, 95, 50);
+  for (let i = 0; i < 0.10*n; i++)
   {
-    ophcurve.push(new Ophiuride(random(175, 225), random(175, 225), 1.5, b, 50, random(-180,180), c1, 2));
+    ophcurve.push(new Ophiuride(random(375, 425), random(175, 225), 1.5, b, 50, random(-180,180), c1, 2));
   }
-  for (let i = 0.15*n + 1; i < n*0.5; i++)
+  for (let i = 0.10*n + 1; i < n*0.4; i++)
   {
-    ophcurve.push(new Ophiuride(random(175, 225), random(175, 225), 1.0, b, 100, random(-180,180), c2, 3));
+    ophcurve.push(new Ophiuride(random(375, 425), random(175, 225), 1.0, b, 100, random(-180,180), c2, 3));
   }
-  for (let i = n*0.5 + 1; i < n*0.8; i++)
+  for (let i = n*0.4 + 1; i < n*0.8; i++)
   {
-    ophcurve.push(new Ophiuride(random(175, 225), random(175, 225), 1.0, b, 150, random(-180,180), c3, 6));
+    ophcurve.push(new Ophiuride(random(375, 425), random(175, 225), 1.0, b, 150, random(-180,180), c3, 9));
   }
   for (let i = n*0.8 + 1; i < n; i++)
   {
-    ophcurve.push(new Ophiuride(random(175, 225), random(175, 225), 1.0, b, 300, random(-180,180), c4, 8));
+    ophcurve.push(new Ophiuride(random(375, 425), random(175, 225), 1.0, b, 300, random(-180,180), c4, 12));
   }
  
 }
 
 function draw() {
-  //background(15,16,32);
+  background(15,16,32);
   //background(253,245,191);
-  background(10,9,8);
+  //background(10,9,8);
   for (let i = ophcurve.length-1; i > 0; i--)
   {
     ophcurve[i].oneCurve();

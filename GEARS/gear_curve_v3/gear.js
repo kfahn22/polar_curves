260f562100ec7532b583b33dee9dc6137fa99482
Kathy <65121394+kfahn22@users.noreply.github.com>
2023-06-11 06:55:56 -0400

    commit latest changes

--- GEARS/gear_curve_v3/gear.js
+++ GEARS/gear_curve_v3/gear.js
@@ -2,7 +2,7 @@
 // https://help.tc2000.com/m/69445/l/755460-hyperbolic-functions-table
 
 class Gear {
-  constructor(_px, _py, _a, _b, _sc, _rot, _c, _st) {
+  constructor(_px, _py, _a, _b, _sc, _rot) {
     this.px = _px;
     this.py = _py;
     this.a = _a;
@@ -11,15 +11,14 @@ class Gear {
     this.sc = _sc;
     this.points = [];
     this.rot = _rot;
-    this.st = _st;
-    this.c = _c;
-    this.col = color(this.c);
+    this.h = [];
+    //this.col = color(this.h, 100, 50, 80);
   }
 
   hyperbolicTan(theta) {
     let e = 2.71828;
-    let l = pow(e, 2*theta);
-    return (l - 1 )/ (l + 1);
+    let l = pow(e, 2 * theta);
+    return (l - 1) / (l + 1);
   }
   // We need to loop through curve once before creating object
   oneCurve() {
@@ -31,12 +30,18 @@ class Gear {
       let x = this.sc * r * sin(theta);
       let y = this.sc * r * cos(theta);
       let p = createVector(x, y);
-      if (this.points.length < 360) {
+
+      if (this.points.length < 361) {
         this.points[theta] = p;
+        //this.h.push(sin(theta));
       } else {
         break;
       }
     }
+  }
+
+  erase() {
+    this.points = [];
   }
 
   show() {
@@ -45,9 +50,12 @@ class Gear {
     translate(this.px, this.py);
     //rotate(this.rot);
     beginShape();
-    for (let v of this.points) {
+    for (let i = 0; i < this.points.length; i++) {
       strokeWeight(2);
-      stroke(this.col);
+      let col = color(220, 100, 50, 80);
+      //let col = color(this.h[i], 100, 50, 80);
+      stroke(col);
+      let v = this.points[i];
       vertex(v.x, v.y);
     }
     endShape();
--- GEARS/gear_curve_v3/sketch.js
+++ GEARS/gear_curve_v3/sketch.js
@@ -6,8 +6,8 @@
 const gcurve = [];
 let a = 1;
 let b = 10;
-let sc = 20;
-let n = 100;
+let sc = 30;
+let n = 2;
 let h = 180;
 let l = 40; 
 let alpha = 100;
@@ -16,7 +16,9 @@ function setup() {
   createCanvas(600, 600);
   colorMode(HSL, 360, 100, 100, 100);
   angleMode(DEGREES);
- 
+  for (let i = 0; i < 4; i++) {
+    gcurve.push(new Gear(width / 2, height / 2, a, b, sc + i * 10, 30, 3));
+  }
   // let c2 = color(250, 166, 255, 150);
   // let c3 = color(115, 83, 186, 75);
   // let c4 = color(47, 25, 95, 50);
@@ -44,26 +46,29 @@ function setup() {
 }
 
 function draw() {
-  background(0, 100, 0, 100);
-   let c1 = color(h, 100, 50, alpha);
-  gcurve.push(new Gear(width / 2, height / 2, a, b, sc, 30, c1, 3));
-  // for (let i = 0; i < 0.15 * n; i++) {
-  //   gcurve.push(new Gear(width / 2, height / 2, a, b, sc, 30, c1, 2));
-  // }
-  for (let i = 0; i < gcurve.length; i++) {
-    gcurve[i].oneCurve();
+  background(280, 100, 80, 100);
+  // let c1 = color(h, 100, 50, alpha);
+ 
+
+  for (let i = 0 ; i < gcurve.length; i++)
+  { gcurve[i].oneCurve();
     gcurve[i].show();
-    
-  }
-  sc += 10;
-  if (sc === 200) {
-    sc = 20;
-  }
-  h += 6;
-  l += 5;
-  alpha -= 3;
+   }
+  //sc = resetScale(sc);
+  // h += 6;
+  // l += 5;
+  // alpha -= 3;
+  //sc += 10;
 }
 
 function mousePressed() {
   save("gear.jpg");
+}
+
+function resetScale(sc, i) {
+  if (sc == 200) {
+    sc = 20;
+    //gcurve[0] = [];
+  }
+  return sc;
 }

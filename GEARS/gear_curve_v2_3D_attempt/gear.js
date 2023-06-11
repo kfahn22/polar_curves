ce85133e9525a17cee931aed2340eeec33da087b
Kathy <65121394+kfahn22@users.noreply.github.com>
2023-06-11 07:06:46 -0400

    commit latest updates

--- GEARS/gear_curve_v2_3D_attempt/gear.js
+++ GEARS/gear_curve_v2_3D_attempt/gear.js
@@ -12,6 +12,7 @@ class Gear {
     this.points = [];
     this.rot = _rot;
     this.h = [];
+    //this.col = color(this.h, 100, 50, 80);
   }
 
   hyperbolicTan(theta) {
@@ -19,17 +20,17 @@ class Gear {
     let l = pow(e, 2 * theta);
     return (l - 1) / (l + 1);
   }
-  
   // We need to loop through curve once before creating object
   oneCurve() {
     for (let theta = 0; theta < 361; theta += 1) {
-      // Equations for gear curve
+      // Equations for Gear curve
       let r =
         this.a +
         (1 / this.b) * this.hyperbolicTan(this.b * sin(this.sp * theta));
       let x = this.sc * r * sin(theta);
       let y = this.sc * r * cos(theta);
-      let p = createVector(x, y);
+      let z = -1;
+      let p = createVector(x, y, z);
 
       if (this.points.length < 361) {
         this.points[theta] = p;
@@ -39,23 +40,22 @@ class Gear {
     }
   }
 
-  erase() {
-    this.points = [];
-  }
-
   show() {
     push();
     noFill();
     translate(this.px, this.py);
+    rotate(this.rot);
     beginShape();
     for (let i = 0; i < this.points.length; i++) {
       strokeWeight(2);
       let col = color(220, 100, 50, 80);
+      //let col = color(this.h[i], 100, 50, 80);
       stroke(col);
       let v = this.points[i];
-      vertex(v.x, v.y);
+      vertex(v.x, v.y, v.z);
     }
     endShape();
     pop();
+    this.points = [];
   }
 }
